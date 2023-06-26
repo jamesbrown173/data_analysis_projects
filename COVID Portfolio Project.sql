f028e0d9fc85c8ee260d08dc40ccb324b4236ff3
jamesbrown173 <93264226+jamesbrown173@users.noreply.github.com>
2023-06-26 15:00:50 +0700

    Update COVID Portfolio Project.sql

--- COVID Portfolio Project.sql
+++ COVID Portfolio Project.sql
@@ -1,16 +1,15 @@
-/* COVID-19 Data Exploration
+--COVID-19 Data Exploration
 
-Skills used: Create tables, import data into table, Joins, CTE's, Temple Tables, Aggregate Functions, Creating Views
+--Skills used: Create tables, import data into table, Joins, CTE's, Temple Tables, Aggregate Functions, Creating Views
 
-*/
 
+---------------------------------------------------------------------------------------------
 -- Create the Tables
 
-/* 
-
+/*
 I'm going to be using two separate datasets. Here I am creating the tables for each dataset.
-
 */
+
 
 CREATE TABLE CovidDeaths(
     iso_code VARCHAR(255),
@@ -89,22 +88,18 @@ CREATE  TABLE CovidVaccinations(
     );
 
 
-
+---------------------------------------------------------------------------------------------
 -- Load Data Infile
 
 /* 
-
 Now the tables have been created I will import the data from the CSV files into the tables using the LOAD DATA INFILE function.
-
 */
 
 
 /* 
-
 One challenge I had during the import process was that the null values where set to "''" rather than "NULL". This caused an error why attempting to import.
 To solve this I convert the "''" entries to "NULL" in Excel before importing the data. 
 I kept back ups of the original CSV files incase there were any errors.
-
 */
 
 LOAD DATA INFILE 'CovidDeaths.csv'
@@ -122,9 +117,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* 
-
 Below are the actual files I imported after correcting the NULL values 
-
 */
 
 LOAD DATA INFILE 'CovidDeathsNullValues.csv'
@@ -142,6 +135,9 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 
 
+
+    
+---------------------------------------------------------------------------------------------
 -- Selecting the imported data to check it it's imported correctly.
 
 SELECT *
@@ -154,7 +150,9 @@ FROM `PortfolioProject`.`CovidVaccinations`
 ORDER BY 3,4
 LIMIT 100
 
+    
 
+---------------------------------------------------------------------------------------------
 -- Select Data that we are going to be using
 
 SELECT Location, Date, total_cases, new_cases, total_deaths, population
@@ -162,22 +160,41 @@ FROM `PortfolioProject`.`CovidDeaths`
 ORDER BY 1,2
 
 
+
+
+---------------------------------------------------------------------------------------------
 -- Total Cases vs Total Deaths for the UK
--- Shows the likelihood of dying if you contract COVID-19 in your country
+
+/*
+Shows the likelihood of dying if you contract COVID-19 in your country
+*/
 
 SELECT Location, Date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
 FROM `PortfolioProject`.`CovidDeaths`
 WHERE Location LIKE '%kingdom%'
 ORDER BY 1,2
 
+
+
+
+---------------------------------------------------------------------------------------------
 -- Total Cases vs Population
--- Shows what percentage of population of the UK got COVID-19
+
+/*
+Shows what percentage of population of the UK got COVID-19
+*/
+
 
 SELECT Location, Date, total_cases, population, (total_cases/population)*100 AS PercentOfPopInfected
 FROM `PortfolioProject`.`CovidDeaths`
 WHERE Location LIKE '%kingdom%'
 ORDER BY 1,2
 
+
+
+
+
+---------------------------------------------------------------------------------------------
 -- Countries with Highest Infection Rate compared to Population
 
 SELECT Location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS PercentOfPopInfected
@@ -186,12 +203,14 @@ GROUP BY Location, population
 ORDER BY PercentOfPopInfected DESC
 
 
+
+
+---------------------------------------------------------------------------------------------
 -- Countries with the Highest Death Count per Population
 
+
 /* 
-
 Here we added the function where continent is not null to remove grouped continent and demogrpahic entries. 
-
 */
 
 SELECT Location, MAX(total_deaths) AS TotalDeathsCount 
@@ -204,9 +223,11 @@ LIMIT 1000
 
 
 
-
+---------------------------------------------------------------------------------------------
 -- Breaking things down by continent
 
+
+---------------------------------------------------------------------------------------------
 -- Continents with the highest Death Count per Population
 
 SELECT continent, MAX(total_deaths) AS TotalDeathsCount 
@@ -221,16 +242,20 @@ ORDER BY TotalDeathsCount DESC
 
 
 
-
+---------------------------------------------------------------------------------------------
 -- Moving onto the CovidVaccinations table
 
 
+---------------------------------------------------------------------------------------------
 -- Checking the CovidVaccinations table
 SELECT *
 FROM `CovidVaccinations`
 LIMIT 1000
 
 
+
+
+---------------------------------------------------------------------------------------------
 -- Joining the two tables
 
 SELECT *
@@ -241,6 +266,9 @@ JOIN `CovidVaccinations` AS VAC
 
 
 
+
+
+---------------------------------------------------------------------------------------------
 -- Looking at Total Population vs Vaccinations
 
 SELECT DEA.continent, DEA.location, DEA.date, DEA.population, VAC.new_vaccinations
@@ -252,6 +280,9 @@ WHERE DEA.continent IS NOT NULL
 ORDER BY 2,3
 
 
+
+
+---------------------------------------------------------------------------------------------
 -- New vaccinations per Day
 
 SELECT DEA.continent, DEA.location, DEA.date, DEA.population, VAC.new_vaccinations
@@ -265,6 +296,8 @@ ORDER BY 2,3
 
 
 
+
+---------------------------------------------------------------------------------------------
 -- Using a CTE to perform a calculation on PARTITION BY in pervious query
 
 WITH PopVsVac (continent, Location, Date, Population,New_Vaccinations, RollingPeopleVaccinated)
@@ -282,6 +315,9 @@ SELECT *, (RollingPeopleVaccinated/Population) *100
 FROM PopVsVac
 
 
+
+
+---------------------------------------------------------------------------------------------
 -- Using Temp Table to perform Calculation on Partition By in previous query
 
 DROP TABLE IF EXISTS PercentPopulationVaccinated
@@ -309,6 +345,9 @@ FROM PercentPopulationVaccinated
 LIMIT 100000
 
 
+
+
+---------------------------------------------------------------------------------------------
 -- Creating View to store data for later visualizations
 
 CREATE VIEW PercentPopulationVaccinated AS
